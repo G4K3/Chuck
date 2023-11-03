ab56701f945bc790fb0b4eda5976d625dabdb105
G4K3 <149729869+G4K3@users.noreply.github.com>
2023-11-02 21:30:04 -0400

    Update Automated-mapping.py

--- Automated-mapping.py
+++ Automated-mapping.py
@@ -221,4 +221,4 @@ if __name__ == '__main__':
 
 st.write('---')
 st.markdown(hide_devcontainer_style, unsafe_allow_html=True)
-st.write('Auteur: [Rablab](https://rablab.ca) | Agence de performance SEO - SEO performance agency | [Contact](https://rablab.ca/contact/)️⚡')
+st.write('Auteur: [Rablab](https://rablab.ca) | Agence de performance SEO | [Contactez-nous!](https://rablab.ca/contact/)️⚡')
